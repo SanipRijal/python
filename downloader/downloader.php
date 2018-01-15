cc9b350125c77beb447775b91bd0721b99e3417f
Aayush Sharma <sharmagiriaayush@gmail.com>
2018-01-15 13:08:54 +0545

    Annotations

--- downloader/downloader.php
+++ downloader/downloader.php
@@ -5,9 +5,9 @@
 </head>
 <body>
 	<p><h1>Neuclear Nadal Downloader</h1></p>
-	<form name = f action = download.php method = 'GET'>
-		<label for="song link">Enter the song link</label>
-		<input type = "text" name = "txtLink" placeholder = "Enter the download link" />
+	<form name ='f' action = 'download.php' method = 'GET'>
+		<label for='song link'>Enter the song link</label>
+		<input type = 'text' name = 'txtLink' placeholder = "Enter the download link" />
 		<br><input type = submit value = "Get Song">
 	</form>
 
